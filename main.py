d08052fb2e389dfaa3d4fceeb39dd199b2f94abd
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2023-06-13 17:44:38 +0200

    Refactor code and improve command handling logic

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from telegraph import Telegraph
-import openai
-import logging
-from aiogram import *
-from aiogram.dispatcher import Dispatcher
 import os
-from LGCH import *
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+import openai
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
+NUM_IMAGES = 3
 
 help_ = """Example of input data from user - 
 <b>/image Portret of Elon Musk</b>
@@ -22,14 +20,19 @@ Available commands:
 /deep_math - default model but with access to wolfram alpha and math llm
 """
 
-telegraph = Telegraph()
-telegraph.create_account(short_name='1337')
-
-NUM_IMAGES = 3
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "/start"}
+    ]
+)
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
+
 
 def get_image(prompt, num_images=NUM_IMAGES):
     try:
@@ -40,52 +43,55 @@ def get_image(prompt, num_images=NUM_IMAGES):
         return None
 
 
-@dp.message_handler(commands = ['web','wiki','deep_math','default'])
+@dp.message_handler(commands=['web', 'wiki', 'deep_math', 'default'])
 async def process_text_generation_command(message: types.Message):
     logging.info("text_generation command received")
     command = message.get_command()
     await bot.send_message(chat_id=message.chat.id, text='Loading...')
     prompt = message.text.replace(command, "")
     text = ''
-    auto = {
-        '/web': load_tools(["google-serper", 'human'], llm=llm),
-        '/summary': load_tools(["google-serper", 'human'], llm=llm),
-        '/wiki': load_tools(["wikipedia", 'human'], llm=llm),
-        '/deep_math': load_tools(['wolfram-alpha', 'human',"llm-math"], llm=llm),
-        '/default': load_tools(['human'], llm=llm)
-    }
-    tools = auto[command]
+    tools = []
+    
+    if command == '/web':
+        tools = ["google-serper", 'human']
+    elif command == '/wiki':
+        tools = ["wikipedia", 'human']
+    elif command == '/deep_math':
+        tools = ['wolfram-alpha', 'human', "llm-math"]
+    elif command == '/default':
+        tools = ['human']
+
     try:
-        text = langchain_answer(prompt, tools) if len(
-            prompt) else 'Invalid format. Write info after function'
+        text = langchain_answer(prompt, tools) if len(prompt) else 'Invalid format. Write info after function'
     except Exception as e:
-        text = f'sorry... sth went wrong {e}'
+        text = f'sorry... something went wrong {e}'
+    
     await bot.send_message(chat_id=message.chat.id, text=text, parse_mode="HTML")
-
-
 
 
 @dp.message_handler(commands=['image'])
 async def process_image_command(message: types.Message):
     logging.info("image command received")
     prompt = message.text.replace("/image", "")
-    image_url = get_image(prompt, m = 3) if len(
-        prompt) else 'Invalid format. Write info after function'
-    if image_url is None:
+    image_urls = get_image(prompt, num_images=NUM_IMAGES) if len(prompt) else 'Invalid format. Write info after function'
+    
+    if image_urls is None:
         await bot.send_message(chat_id=message.chat.id, text="An error occurred while generating the image.")
-    for x in range(3):
-        await bot.send_photo(chat_id=message.chat.id,photo=image_url[x], caption=prompt)
+    
+    for url in image_urls:
+        await bot.send_photo(chat_id=message.chat.id, photo=url, caption=prompt)
+
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     logging.info("/start command received")
-    await bot.send_message(chat_id=message.chat.id, text="Please use /help to see next steps. \nAlso you can use /about to see info about bot.")
+    await bot.send_message(chat_id=message.chat.id, text="Please use /help to see next steps. \nAlso, you can use /about to see info about the bot.")
 
 
 @dp.message_handler(commands=['about'])
 async def process_about_command(message: types.Message):
     logging.info("/about command received")
-    await bot.send_message(chat_id=message.chat.id, text="This is an example bot that generates images, text, translate, summarize based on a user's description. It uses OpenAI's image generation, text generation, translation and summarization API.")
+    await bot.send_message(chat_id=message.chat.id, text="This is an example bot that generates images and text based on a user's description. It uses OpenAI's image generation and text generation API.")
 
 
 @dp.message_handler(commands=['help'])
